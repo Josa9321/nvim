99940f5b5a5f62dd26aab9f1ea6b4cd46041ac4a
Josa9321 <josenildo.junior9321@gmail.com>
2025-11-27 19:36:38 -0300

    feat: set cmp plug to autocomplete languages

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,80 @@
+-- Set up nvim-cmp.
+local cmp = require('cmp')
+
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+
+            -- For `mini.snippets` users:
+            -- local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
+            -- insert({ body = args.body }) -- Insert at cursor
+            -- cmp.resubscribe({ "TextChangedI", "TextChangedP" })
+            -- require("cmp.config").set_onetime({ sources = {} })
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'vsnip' }, -- For vsnip users.
+        -- { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+-- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
+-- Set configuration for specific filetype.
+--[[ cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+        { name = 'git' },
+    }, {
+        { name = 'buffer' },
+    })
+})
+require("cmp_git").setup() ]]--
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- lua lang
+
 vim.lsp.config['lua_ls'] = {
       -- Command and arguments to start the server.
     cmd = { '/data/data/com.termux/files/usr/bin/lua-language-server' },
@@ -9,6 +86,7 @@ vim.lsp.config['lua_ls'] = {
     root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
     -- Specific settings to send to the server. The schema is server-defined.
     -- Example: https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+    capabilities=capabilities,
     settings = {
         Lua = {
             runtime = {
@@ -26,6 +104,7 @@ vim.lsp.config("pyright", {
         "/data/data/com.termux/files/usr/bin/pyright-langserver",
         "--stdio",
 	},
+    capabilities=capabilities,
     filetypes = {'python'}
 })
 
@@ -40,6 +119,7 @@ vim.lsp.config("clangd", {
         '--background-index', '--clang-tidy', '--log=verbose'
 	},
     root_markers = { '.clangd', 'compile_commands.json' },
+    capabilities=capabilities,
     filetypes = {'c', 'cpp', 'h', 'hpp'}
 })
 
