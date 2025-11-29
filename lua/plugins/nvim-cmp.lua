39b6fa4de71e4a0bdb2b1b97456e56a1751996d0
Josa9321 <josenildo.junior9321@gmail.com>
2025-11-29 11:22:28 -0300

    change: set tab to go to next option luasnip

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,8 +1,13 @@
 return {
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path'},
-    {'hrsh7th/cmp-cmdline'},
+    {
+        'neovim/nvim-lspconfig',
+    dependencies = {
+         'hrsh7th/nvim-cmp' ,
+         'hrsh7th/cmp-nvim-lsp' ,
+         'hrsh7th/cmp-buffer' ,
+         'hrsh7th/cmp-path' ,
+         'hrsh7th/cmp-cmdline' ,
+	'saadparwaiz1/cmp_luasnip'
+}
+} 
 }
