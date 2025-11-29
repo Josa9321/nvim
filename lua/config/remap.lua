66f1b1f18affb23390f3680761c6bbb5fd15c11d
Josa9321 <josenildo.junior9321@gmail.com>
2025-11-29 19:54:44 -0300

    change: set some useful remaps to navigate through the nvim windows, and change img from dashboard

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -85,3 +85,13 @@ vim.keymap.set("n", "<leader>rc",
     ":w<CR>:!clang++ % -o %:r && %:r <CR>",
     { silent = true }
 )
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>")
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>")
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -5<cr>")
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +5<cr>")
