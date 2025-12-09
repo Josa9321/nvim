c15c2d46182fbc768b02db5e0f66b0e54f18dcd6
Josa9321 <josenildo.junior9321@gmail.com>
2025-12-09 00:15:00 -0300

    feat: set specific filetype rules for tex files, some characters as break lines to display with cyclist, and a manual inside nvim for cpp

---
+++ after/ftplugin/tex.lua
@@ -0,0 +1,18 @@
+local opt = vim.opt_local
+local map = vim.keymap.set
+
+opt.wrap = true
+
+-- Buffer-local tweaks for TeX files
+opt.spell = true
+opt.spelllang = "en_us"
+
+
+opt.tabstop=4
+opt.sw=4
+opt.linebreak=true
+opt.breakindent=true
+
+map('n', '<Down>', 'gj')
+map('n', '<Up>', 'gk')
+
--- lua/ftplugin/tex.lua
+++
@@ -1,8 +0,0 @@
-local opt = vim.opt
-
-opt.wrap = true
-
--- Buffer-local tweaks for TeX files
-vim.opt_local.conceallevel = 2 -- keep math pretty but text readable
-vim.opt_local.spell = true
-vim.opt_local.spelllang = "en_us"
