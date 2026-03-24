73bfa496c42263d7cccd2fe2b0b1f74ce4aa9a71
Josa9321 <josenildo.junior9321@gmail.com>
2026-03-24 09:01:50 -0300

    change: set JuliaLS cmd to be equal to docs

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -182,28 +182,19 @@ vim.lsp.enable("clangd")
 vim.lsp.config('julials', {
     cmd = {
         "julia",
-        "--project",
         "--startup-file=no",
         "--history-file=no",
         "-e", [[
-            using Pkg
-            Pkg.instantiate()
             using Revise
             using LanguageServer
-            project_path = let
-                dirname(something(
-                    Base.load_path_expand((
-                        p = get(ENV, "JULIA_PROJECT", nothing);
-                            p === nothing ? nothing : isempty(p) ? nothing : p
-                        )),
-                            Base.current_project(),
-                            get(Base.load_path(), 1, nothing),
-                        Base.load_path_expand("@v#.#"),
-                    ))
-            end
-            Pkg.activate(project_path)
-            push!(LOAD_PATH, project_path)
-            server = LanguageServer.LanguageServerInstance(stdin, stdout, project_path)
+            using Pkg
+
+            import StaticLint
+            import SymbolServer
+
+            env_path = dirname(Pkg.Types.Context().env.project_file)
+            server = LanguageServer.LanguageServerInstance(stdin, stdout, env_path, "");
+            server.runlinter = true;
             run(server)
         ]]
     },
